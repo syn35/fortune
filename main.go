46c180c5bc882955590cec42c587fb895ba5291b
jace <jace@prakashs-air.attlocal.net>
2017-08-05 11:54:13 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+func main() {
+	fmt.Printf("%s\n", "jace is here")
+	fmt.Printf("%s\n", "what did the tree wear to the pool? swimming trunks!")
+	fmt.Printf("%s\n", "when nothing goes right, go left!")
+	fmt.Printf("%s\n", "I don't need a hair stylist, my pillow gives me a new hairstyle evevry morning")
+	fmt.Printf("%s\n", "my computer once beat me at chess. but it was no match for me at kick boxing")
+	fmt.Printf("%s\n", "my room is like the bermuda triangle, stuff goes in and is never seen again")
+	fmt.Printf("%s\n", "life always offers you a second chance. it's called tommorow.")
+	fmt.Printf("%s\n", "my bed is a magical place where I suddenly remember everything I forgot to do")
+	fmt.Printf("%s\n", "the future is shaped by your dreams, so at night stop wasting time and go to sleep")
+	fmt.Printf("%s\n", "they say don't try this at home, so I am coming over to your house to try it.")
+	fmt.Printf("%s\n", "lazy people fact #39841297423, you were too lazy to read that number.")
+}
